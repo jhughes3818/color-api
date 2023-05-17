bce166e505d28ee0a957d7c5cbc483d34aaccee7
Jason <jhughes3818@gmail.com>
2023-05-17 16:10:30 +1000

    add files

---
+++ src/pages/api/get-colors.js
@@ -0,0 +1,40 @@
+import Vibrant from "node-vibrant";
+import sharp from "sharp";
+
+export default async function handler(req, res) {
+  const imageUrl = req.query.url;
+
+  if (!imageUrl) {
+    return res.status(400).json({ error: "Missing image URL" });
+  }
+
+  try {
+    //const convertedImageUrl = await convertToPNG(imageUrl);
+    const color = await getColorFromImage(imageUrl);
+    res.status(200).json({ color });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Error processing the image" });
+  }
+}
+
+async function convertToPNG(imageUrl) {
+  const convertedImageBuffer = await sharp(imageUrl).toFormat("png").toBuffer();
+  const convertedImageUrl = `data:image/png;base64,${convertedImageBuffer.toString(
+    "base64"
+  )}`;
+  return convertedImageUrl;
+}
+
+async function getColorFromImage(imageUrl) {
+  const palette = await Vibrant.from(imageUrl).getPalette();
+  const dominantColor =
+    palette.Vibrant ||
+    palette.Muted ||
+    palette.DarkVibrant ||
+    palette.DarkMuted ||
+    palette.LightVibrant ||
+    palette.LightMuted;
+
+  return dominantColor.hex;
+}

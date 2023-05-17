3f3abd54d69530a43f4c1ad1bb849058b1e79932
Jason <jhughes3818@gmail.com>
2023-05-17 16:23:14 +1000

    return dark vibrant

--- src/pages/api/get-colors.js
+++ src/pages/api/get-colors.js
@@ -29,10 +29,10 @@ async function convertToPNG(imageUrl) {
 async function getColorFromImage(imageUrl) {
   const palette = await Vibrant.from(imageUrl).getPalette();
   const dominantColor =
+    palette.DarkVibrant ||
     palette.LightMuted ||
     palette.Muted ||
     palette.Vibrant ||
-    palette.DarkVibrant ||
     palette.DarkMuted ||
     palette.LightVibrant;
   return dominantColor.hex;
